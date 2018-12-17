a7308182768b0b974c4017641b0608e9b9f20071
Erna Oudman <ernaoudman@hotmail.com>
2018-12-18 00:09:17 +0100

    day 14, almost

---
+++ 2018/day14.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+def get_next(r_list, recipes, l1,l2):
+    # while list(recipes) != r_list[-7:]:
+    while len(r_list) <= int(recipes)+10:
+        newval = str(int(r_list[l1]) + int(r_list[l2]))
+        r_list += list(newval)
+        l1 = (int(r_list[l1])+l1+1) % len(r_list)
+        l2 = (int(r_list[l2])+l2+1) % len(r_list)
+        
+    else:
+        return ''.join(r_list[int(recipes):int(recipes)+10])
+        #return r_list
+    
+start =  '37'
+recipes = '030121'
+
+a = get_next(list(start),recipes,0,1)
+print(a)
+
+def get_index(r_list, recipes, l1,l2):
+    # while list(recipes) != r_list[-7:]:
+    while r_list[-len(recipes):] != list(recipes):
+        newval = str(int(r_list[l1]) + int(r_list[l2]))
+        r_list += list(newval)
+        l1 = (int(r_list[l1])+l1+1) % len(r_list)
+        l2 = (int(r_list[l2])+l2+1) % len(r_list)
+        
+    else:
+        return len(r_list) - len(recipes)
+
+    
+b = get_index(list(start), recipes,0,1)
+print(b)
